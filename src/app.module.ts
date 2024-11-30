14d36426ee5c0dbfc69d17111a1c7520374dbd9b
Smae Tongmenglay <smaetongmenglay@MENGLAY.local>
2024-11-30 13:00:05 +0700

    ..

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
+import { AuthService } from './auth/auth.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, AuthModule],
+  controllers: [AuthController],
+  providers: [AuthService],
 })
 export class AppModule {}
